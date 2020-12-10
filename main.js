868b1d6287c84abe704d2e4d6e06f83390a3f128
Nathalie Larssongit config --listgit config --global user.email nathalienlarsson@gmail.com <nathalienlarsson@gmail.comgit config --listgit config --global user.email nathalienlarsson@gmail.com>
2020-12-10 11:16:16 +0100

    Finally maybe done

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-    $("#imgGrow").hover(
+    $(".imgGrow").hover(
         function(){$(this).animate({width: "370px", height:"355px"}, 1000);},        
         function(){$(this).animate({width: "270px", height:"255px"}, 1000);} 
 );
